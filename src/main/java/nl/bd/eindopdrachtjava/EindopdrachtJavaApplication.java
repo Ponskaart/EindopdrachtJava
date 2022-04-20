53838693f7f3a7dafa44dead1fdb1a03024ccdc1
Ponskaart <95726416+Ponskaart@users.noreply.github.com>
2022-04-20 22:12:42 +0200

    Eerste commit
    Eerste commit, project aangemaakt.

---
+++ src/main/java/nl/bd/eindopdrachtjava/EindopdrachtJavaApplication.java
@@ -0,0 +1,13 @@
+package nl.bd.eindopdrachtjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/bd/eindopdrachtjava/EindopdrachtJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.bd.eindopdrachtjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
