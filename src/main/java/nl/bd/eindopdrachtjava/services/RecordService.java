689c195df9288ea9cc6a182c693bab7b9f5fdbaa
Ponskaart <95726416+Ponskaart@users.noreply.github.com>
2022-04-25 21:41:16 +0200

    Updated RecordService
    registerRecord now also adds the artist object to the record entity.

--- src/main/java/nl/bd/eindopdrachtjava/services/RecordService.java
+++ src/main/java/nl/bd/eindopdrachtjava/services/RecordService.java
@@ -3,6 +3,7 @@ package nl.bd.eindopdrachtjava.services;
 import lombok.AllArgsConstructor;
 import nl.bd.eindopdrachtjava.models.entityModels.Record;
 import nl.bd.eindopdrachtjava.models.requests.RecordRegistrationRequest;
+import nl.bd.eindopdrachtjava.repositories.ArtistRepository;
 import nl.bd.eindopdrachtjava.repositories.RecordRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,16 +16,23 @@ import java.util.List;
 @AllArgsConstructor
 public class RecordService {
     private RecordRepository recordRepository;
+    private ArtistRepository artistRepository;
 
     /**
-     *method retrieves all Record entities from the database and returns them as a list.
+     *Method retrieves all Record entities from the database and returns them as a list.
      */
     public List<Record> getAllRecords() {
         return recordRepository.findAll();
     }
 
+    /**
+     * Method to register a new record using the builder design pattern to make it easier to see what is actually
+     * happening. I'm also using a wrapper class when registering the record so that I dont have to pass this method 9
+     * different variables.
+     */
     public Record registerRecord(RecordRegistrationRequest recordRegistrationRequest){
         Record record = Record.builder()
+                .artist(artistRepository.findArtistByArtistName(recordRegistrationRequest.getArtistName()))
                 .title(recordRegistrationRequest.getTitle())
                 .genre(recordRegistrationRequest.getGenre())
                 .label(recordRegistrationRequest.getLabel())
