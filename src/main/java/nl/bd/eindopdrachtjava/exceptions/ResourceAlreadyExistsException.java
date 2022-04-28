feafad5565228c8dfca45a3cfaf73599d4e28366
Ponskaart <95726416+Ponskaart@users.noreply.github.com>
2022-04-28 12:41:57 +0200

    added comments to exceptions

--- src/main/java/nl/bd/eindopdrachtjava/exceptions/ResourceAlreadyExistsException.java
+++ src/main/java/nl/bd/eindopdrachtjava/exceptions/ResourceAlreadyExistsException.java
@@ -3,6 +3,10 @@ package nl.bd.eindopdrachtjava.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom exception class that is called when an entity already exists within the database. Sends back a http status and
+ * a custom error message.
+ */
 @ResponseStatus(value = HttpStatus.CONFLICT)
 public class ResourceAlreadyExistsException extends RuntimeException{
     public ResourceAlreadyExistsException(String message) {
--- src/main/java/nl/bd/eindopdrachtjava/exceptions/ResourceNotFoundException.java
+++ src/main/java/nl/bd/eindopdrachtjava/exceptions/ResourceNotFoundException.java
@@ -3,6 +3,10 @@ package nl.bd.eindopdrachtjava.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Custom exception class that is called when an entity does not exist within the database. Sends back a http status and
+ * a custom error message.
+ */
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException(String message){
