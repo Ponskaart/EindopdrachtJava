bf9a0e2f722e294de1ec33ffe3cdc4571f85e197
Ponskaart <95726416+Ponskaart@users.noreply.github.com>
2022-05-07 10:22:01 +0200

    added CoverArtService
    Added method to upload an image to the database

--- src/main/java/nl/bd/eindopdrachtjava/services/CoverArtService.java
+++ src/main/java/nl/bd/eindopdrachtjava/services/CoverArtService.java
@@ -4,19 +4,26 @@ import lombok.AllArgsConstructor;
 import nl.bd.eindopdrachtjava.models.entities.CoverArt;
 import nl.bd.eindopdrachtjava.repositories.CoverArtRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+/**
+ * Service layer which handles all image up/downloading related logic.
+ */
 @Service
 @AllArgsConstructor
 public class CoverArtService {
     private CoverArtRepository coverArtRepository;
 
-    public CoverArt uploadCoverArt(){
-        Image dbImage = new Image();
-        dbImage.setName(multipartImage.getName());
-        dbImage.setContent(multipartImage.getBytes());
+    /**
+     * Method saves an image to the database and returns its Id to the user.
+     */
+    public Long uploadCoverArt(MultipartFile multipartImage) throws IOException {
+        CoverArt coverArt = new CoverArt();
+        coverArt.setTitle(multipartImage.getName());
+        coverArt.setContent(multipartImage.getBytes());
 
-        return imageDbRepository.save(dbImage)
-                .getId();
+        return coverArtRepository.save(coverArt).getCoverArtId();
     }
-
 }
