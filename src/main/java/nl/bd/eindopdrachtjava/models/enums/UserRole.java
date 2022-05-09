de72276d0865911799587b604e365a073d5418a1
Ponskaart <95726416+Ponskaart@users.noreply.github.com>
2022-05-09 14:26:40 +0200

    Update UserRole.java
    filled in enum class

--- src/main/java/nl/bd/eindopdrachtjava/models/enums/UserRole.java
+++ src/main/java/nl/bd/eindopdrachtjava/models/enums/UserRole.java
@@ -1,7 +1,26 @@
 package nl.bd.eindopdrachtjava.models.enums;
 
-public enum UserRole{
-    EMPLOYEE,
-    ADMIN,
-    CUSTOMER
+import org.springframework.security.core.GrantedAuthority;
+
+public enum UserRole implements GrantedAuthority {
+    EMPLOYEE(Code.EMPLOYEE),
+    ADMIN(Code.ADMIN),
+    CUSTOMER(Code.CUSTOMER);
+
+    private final String authority;
+
+    UserRole(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        return null;
+    }
+    
+    public class Code {
+        public static final String EMPLOYEE = "ROLE_EMPLOYEE";
+        public static final String ADMIN = "ROLE_ADMIN";
+        public static final String CUSTOMER = "ROLE_CUSTOMER";
+    }
 }
