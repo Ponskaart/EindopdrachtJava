1562b9802632df4ad2960d69533c5c81dee1c0ca
Ponskaart <95726416+Ponskaart@users.noreply.github.com>
2022-05-10 15:29:26 +0200

    Update ArtistController.java
    implemented authorizations

--- src/main/java/nl/bd/eindopdrachtjava/controllers/ArtistController.java
+++ src/main/java/nl/bd/eindopdrachtjava/controllers/ArtistController.java
@@ -3,6 +3,7 @@ package nl.bd.eindopdrachtjava.controllers;
 import lombok.AllArgsConstructor;
 import nl.bd.eindopdrachtjava.models.annotations.AdminAuthorization;
 import nl.bd.eindopdrachtjava.models.annotations.CustomerAuthorization;
+import nl.bd.eindopdrachtjava.models.annotations.EmployeeAuthorization;
 import nl.bd.eindopdrachtjava.models.entities.Artist;
 import nl.bd.eindopdrachtjava.models.enums.UserRole;
 import nl.bd.eindopdrachtjava.models.requests.ArtistRegistrationRequest;
@@ -26,6 +27,7 @@ public class ArtistController {
      * Endpoint creates a new artist object and saves it to te database.
      */
     @AdminAuthorization
+    @EmployeeAuthorization
     @PostMapping("/artist")
     public Artist registerArtist(@RequestBody ArtistRegistrationRequest artistRegistrationRequest){
         return artistService.registerArtist(artistRegistrationRequest);
@@ -34,6 +36,8 @@ public class ArtistController {
     /**
      * Endpoint shows all registered artists.
      */
+    @AdminAuthorization
+    @EmployeeAuthorization
     @CustomerAuthorization
     @GetMapping("/artists")
     public List<Artist> getAllArtists(){
@@ -43,6 +47,9 @@ public class ArtistController {
     /**
      * Endpoint shows all artists established in a specific year.
      */
+    @AdminAuthorization
+    @EmployeeAuthorization
+    @CustomerAuthorization
     @GetMapping("/artists/{established}")
     public List<Artist> getArtistsByYearEstablished(@PathVariable int established){
         return artistService.getArtistsByYearEstablished(established);
@@ -51,6 +58,9 @@ public class ArtistController {
     /**
      * Endpoint shows an artist with a specific Id.
      */
+    @AdminAuthorization
+    @EmployeeAuthorization
+    @CustomerAuthorization
     @GetMapping("/artist/{artistId}")
     public Artist getArtistByArtistId(@PathVariable Long artistId){
         return artistService.getArtistByArtistId(artistId);
@@ -59,6 +69,9 @@ public class ArtistController {
     /**
      * Endpoint to show an artist with a specific name.
      */
+    @AdminAuthorization
+    @EmployeeAuthorization
+    @CustomerAuthorization
     @GetMapping("/artist/{artistName}")
     public Artist getArtistByArtistName(@PathVariable String artistName){
         return artistService.getArtistByArtistName(artistName);
@@ -67,6 +80,7 @@ public class ArtistController {
     /**
      * Endpoint deletes artist with a specific Id.
      */
+    @AdminAuthorization
     @DeleteMapping("/artist/{artistId}")
     public void deleteArtist(@PathVariable Long artistId){
         artistService.deleteArtist(artistId);
