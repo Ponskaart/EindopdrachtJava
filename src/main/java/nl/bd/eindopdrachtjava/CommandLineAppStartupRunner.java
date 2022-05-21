df37d77b0fbedbe4b08e662df894f461d363168d
Ponskaart <95726416+Ponskaart@users.noreply.github.com>
2022-05-21 15:31:15 +0200

    Added testdata
    data added through CommandLineStartupRunner

--- src/main/java/nl/bd/eindopdrachtjava/CommandLineAppStartupRunner.java
+++ src/main/java/nl/bd/eindopdrachtjava/CommandLineAppStartupRunner.java
@@ -1,13 +1,22 @@
 package nl.bd.eindopdrachtjava;
 
 import lombok.AllArgsConstructor;
+import nl.bd.eindopdrachtjava.models.entities.CoverArt;
+import nl.bd.eindopdrachtjava.models.entities.Record;
+import nl.bd.eindopdrachtjava.models.entities.Artist;
 import nl.bd.eindopdrachtjava.models.entities.User;
 import nl.bd.eindopdrachtjava.models.enums.UserRole;
+import nl.bd.eindopdrachtjava.repositories.ArtistRepository;
+import nl.bd.eindopdrachtjava.repositories.CoverArtRepository;
+import nl.bd.eindopdrachtjava.repositories.RecordRepository;
 import nl.bd.eindopdrachtjava.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import java.io.*;
+import java.awt.image.*;
+import javax.imageio.*;
 
 /**
  * Class runs before actual program starts and loads database with an admin user and a customer user.
@@ -18,9 +27,12 @@ public class CommandLineAppStartupRunner {
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
     @Bean
-    CommandLineRunner commandLineRunner(UserRepository userRepository){
+    CommandLineRunner commandLineRunner(UserRepository userRepository,
+                                        ArtistRepository artistRepository,
+                                        RecordRepository recordRepository,
+                                        CoverArtRepository coverArtRepository){
         return args -> {
-            if(userRepository.findByUserRole(UserRole.ADMIN).isEmpty()){
+            if(userRepository.findByUserRole(UserRole.ADMIN).isEmpty()) {
                 User admin = User.builder()
                         .username("Admin")
                         .password(bCryptPasswordEncoder.encode("VeryGoodPassword"))
@@ -29,7 +41,7 @@ public class CommandLineAppStartupRunner {
                 userRepository.save(admin);
             }
 
-            if(userRepository.findByUserRole(UserRole.CUSTOMER).isEmpty()){
+            if(userRepository.findByUserRole(UserRole.CUSTOMER).isEmpty()) {
                 User customer = User.builder()
                         .username("Customer")
                         .password(bCryptPasswordEncoder.encode("EvenBetterPassword"))
@@ -38,7 +50,7 @@ public class CommandLineAppStartupRunner {
                 userRepository.save(customer);
             }
 
-            if(userRepository.findByUserRole(UserRole.EMPLOYEE).isEmpty()){
+            if(userRepository.findByUserRole(UserRole.EMPLOYEE).isEmpty()) {
                 User employee = User.builder()
                         .username("Employee")
                         .password(bCryptPasswordEncoder.encode("TheBestPassword"))
@@ -46,6 +58,43 @@ public class CommandLineAppStartupRunner {
                         .build();
                 userRepository.save(employee);
             }
+
+            if (artistRepository.findById(1L).isEmpty()) {
+                Artist artist = Artist.builder()
+                        .artistName("Ben de Testartiest")
+                        .established(2022)
+                        .build();
+                artistRepository.save(artist);
+            }
+
+            if (recordRepository.findById(1L).isEmpty()) {
+                Record record = Record.builder()
+                        .artist(artistRepository.findById(1L).get())
+                        .title("Hij was maar een testartiest")
+                        .genre("Schlager")
+                        .label("Test Records")
+                        .color("Green")
+                        .year(2022)
+                        .country("Germany")
+                        .price(0.99)
+                        .qtyInStock(99)
+                        .build();
+                recordRepository.save(record);
+            }
+
+            if (coverArtRepository.findById(1L).isEmpty()) {
+                BufferedImage bufferimage = ImageIO.read(new File("src/main/resources/CoverArtTest.PNG"));
+                ByteArrayOutputStream output = new ByteArrayOutputStream();
+                ImageIO.write(bufferimage, "png", output );
+                byte [] data = output.toByteArray();
+
+                CoverArt coverArt = new CoverArt();
+                coverArt.setRecord(recordRepository.findById(1L).get());
+                coverArt.setContent(data);
+
+                coverArtRepository.save(coverArt);
+            }
+
         };
     }
 }
