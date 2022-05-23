7ea096a9544a2e573fd36f676aa55d9f00a43c45
Ponskaart <95726416+Ponskaart@users.noreply.github.com>
2022-05-23 17:51:32 +0200

    Update RecordService.java

--- src/main/java/nl/bd/eindopdrachtjava/services/RecordService.java
+++ src/main/java/nl/bd/eindopdrachtjava/services/RecordService.java
@@ -36,7 +36,7 @@ public class RecordService {
     /**
      * Method retrieves all Record entities from the database and returns them as a list.
      */
-    public List<Record> getAllRecords() {
+    public List<Record> getAllRecords() throws ResourceNotFoundException {
         if ((recordRepository.findAll().isEmpty())) {
             throw new ResourceNotFoundException("No Records were found");
         } else {
